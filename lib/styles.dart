8dbd3fe0772ebbc476c6a3f7ddc9abfd9073d034
Ryan Pollard <71097838+ryanpoll@users.noreply.github.com>
2023-02-05 19:26:24 -0500

    Create styles.dart

---
+++ lib/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class ThemeText {
+  static const TextStyle appBarText = TextStyle(
+      fontFamily: 'Montserrat',
+      color: Colors.white,
+      fontSize: 28,
+      fontWeight: FontWeight.w600);
+}
