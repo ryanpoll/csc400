169104b2a4b0241976dc489414e2b790c9dd1f2f
Ryan Pollard <71097838+ryanpoll@users.noreply.github.com>
2023-02-08 15:28:36 -0500

    Update styles.dart

--- lib/styles.dart
+++ lib/styles.dart
@@ -9,13 +9,7 @@ abstract class ThemeText {
 
   static const TextStyle mainButtonText = TextStyle(
       fontFamily: 'Montserrat',
-      color: Colors.white,
+      color: Color.fromARGB(221, 2, 18, 200),
       fontSize: 17,
-      fontWeight: FontWeight.w600);
-
-  static const IconData map_outlined =
-      IconData(0xf1ae, fontFamily: 'MaterialIcons');
-
-  static const IconData local_restaurant_rounded =
-      IconData(0xf869, fontFamily: 'MaterialIcons');
+      fontWeight: FontWeight.w800);
 }
