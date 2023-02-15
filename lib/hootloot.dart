2d8ec7fc0a634e5af4cad7e6325850196de4591b
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-02-14 19:29:52 -0500

    Update hootloot.dart

--- lib/hootloot.dart
+++ lib/hootloot.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/hootLootOn.dart';
+import 'package:flutter_application_1/hootLootOff.dart';
 
 class HootLoot extends StatelessWidget {
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hoot Loot'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            child: const Text('Back To Home Screen'),
-            // brings back to home screen
-            onPressed: () => Navigator.pop(context)),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 0, 21, 255),
+          title: const Text(
+            'HootLoot',
+          ),
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: const Text('On Campus'),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(220, 70),
+                      backgroundColor: Color.fromARGB(255, 0, 21, 255)),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => HootLootOn()));
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('Off Campus'),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(220, 70),
+                      backgroundColor: Color.fromARGB(255, 0, 21, 255)),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                            builder: (BuildContext context) => HootLoot()));
+                  },
+                ),
+              ]),
+        ));
   }
 }
