0581694cfe11144fa10a18caa7a9445ffd1924e0
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-02-14 19:41:58 -0500

    Create off_campus.dart

---
+++ lib/off_campus.dart
@@ -0,0 +1,146 @@
+class OffCampus {
+  final String storeName;
+  final String address;
+  final String phone;
+
+  const OffCampus({required this.storeName, required this.address, required this.phone});
+}
+
+const kStore = <OffCampus>[
+    OffCampus(
+      storeName: 'Acropolis Diner',
+      address:'1864 Dixwell Ave, Hamden, CT',
+      phone: '203-288-0400'
+    ),
+    OffCampus(
+      storeName: 'Ali Babas Fusion',
+      address:'986 Dixwell Ave, Hamden, CT',
+      phone: '203-535-1164'
+    ),
+    OffCampus(
+      storeName: 'Blake Street Convenience',
+      address:'295 Blake Street, New Haven, CT',
+      phone: '203-691-9064'
+    ),
+    OffCampus(
+      storeName: "Brother Jimmy's BBQ",
+      address:'196 Crown Street, New Haven, CT',
+      phone: '203-535-0843'
+),
+    OffCampus(
+      storeName: 'China Wok',
+      address:'1245 Dixwell Ave, Hamden, CT',
+      phone: '203-288-9403'
+    ),
+    OffCampus(
+      storeName: 'Cilantro Mexican Grill',
+      address:'1158 Whalley Ave, New Haven, CT',
+      phone: '203-389-1212'
+    ),
+    OffCampus(
+      storeName: 'CVS/Pharmacy',
+      address:'1150 Whalley Ave, New Haven, CT',
+      phone: '203-389-4714'
+    ),
+    OffCampus(
+      storeName: 'Dayton Street Apizza',
+      address:'60 Dayton Street, New Haven, CT',
+      phone: '203-389-2454'
+    ),
+    OffCampus(
+      storeName: "Domino's Pizza",
+      address:'357 Whalley Ave, New Haven, CT',
+      phone: '203-624-3317'
+    ),
+   
+    OffCampus(
+      storeName: 'Ixtapa Grill Mexican Restaurant',
+      address:'2547 Whitney Ave Hamden, CT',
+      phone: '203-230-2586'
+    ),
+    OffCampus(
+      storeName: 'Meineke Car Care Center',
+      address:'1175 Whalley Ave, New Haven, CT',
+      phone: '203-397-2353'
+    ),
+    OffCampus(
+      storeName: 'More Than Pizza',
+      address:'283 Blake Street, New Haven, CT',
+      phone: '203-389-2222'
+    ),
+    OffCampus(
+      storeName: 'Neighbors Express Mart',
+      address:'158 Fitch Street, New Haven, CT',
+      phone: '203-397-1320'
+    ),
+    OffCampus(
+      storeName: 'Pizza Heaven II',
+      address:'410 Blake Street, New Haven, CT',
+      phone: '203-764-2426'
+    ),
+    OffCampus(
+      storeName: 'Rite Aid #00441',
+      address:'66 Church Street, New Haven, CT',
+      phone: '203-777-7248'
+    ),
+    OffCampus(
+      storeName: 'Rite Aid #01033',
+      address:'85 Middletown Ave, North Haven, CT',
+      phone: '203-865-3179'
+    ),
+    OffCampus(
+      storeName: 'Rite Aid #01839',
+      address:'70 Elm Street, West Haven, CT',
+      phone: '203-933-5260'
+    ),
+    OffCampus(
+      storeName: 'Rite Aid #01882',
+      address:'325 Ferry Street, New Haven, CT',
+      phone: '203-777-0695'
+    ),
+    OffCampus(
+      storeName: 'Rite Aid #04778',
+      address:'588 Main Street, East Haven, CT',
+      phone: '203-469-7648'
+    ),
+    OffCampus(
+      storeName: 'Rite Aid #06708',
+      address:'249 Legion Avenue, New Haven, CT',
+      phone: '203-492-0090'
+    ),
+    OffCampus(
+      storeName: 'Rite Aid #10375',
+      address:'2175 Dixwell Ave, Hamden, CT',
+      phone: '203-288-3895'
+    ),
+    OffCampus(
+      storeName: 'Spooner Restaurant & Pizza',
+      address:'1400 Whalley Ave, New Haven, CT',
+      phone: '203-389-9894'
+    ),
+    OffCampus(
+      storeName: 'Stop & Shop #2633',
+      address:'150 Whalley Ave, New Haven, CT',
+      phone: '203-503-0105'
+    ),
+     OffCampus(
+      storeName: 'Subway',
+      address:'1461 Whalley Avenue, New Haven, CT',
+      phone: '203-389-1520'
+    ),
+     OffCampus(
+      storeName: 'Three Brothers Diner',
+      address:'1038 Dixwell Ave, Hamden, CT',
+      phone: '203-777-9420'
+    ),
+     OffCampus(
+      storeName: 'White Stone Mini Mart',
+      address:'144 Arch Street, Hamden, CT',
+      phone: '203-772-4558'
+    ),
+     OffCampus(
+      storeName: 'Wing Madness',
+      address:'716 Dixwell Ave, New Haven, CT',
+      phone: '203-562-1188'
+    )
+  ];
