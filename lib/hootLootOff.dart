fa492b22014116368276977ec1d5e8c7a077d6df
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-02-27 19:46:14 -0500

    Update hootLootOff.dart

--- lib/hootLootOff.dart
+++ lib/hootLootOff.dart
@@ -1,54 +1,35 @@
 import 'package:flutter/material.dart';
+import 'styles.dart';
 import 'off_campus.dart';
+import 'store_details_off.dart';
 
-class HootLoot extends StatelessWidget {
 
+
+class HootLootOff extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 0, 21, 255),
-          title: Text("Off-Campus Locations"),
+          title: Text('Off Campus'),
         ),
-        body: OffList(kStore)
-      );
+        body: ListView.builder(
+            itemCount: storeList.length,
+            itemBuilder: (context, index) {
+              OffCampus store = storeList[index];
+              return Card(
+                child: ListTile(
+                  title: Text(store.name),
+                  subtitle: Text(store.address.toString()),
+                  leading: Image.network(store.imageUrl),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StoreDetailsScreen(store)));
+                  },
+                ),
+              );
+            }));
   }
-
-}
-
-class OffList extends StatelessWidget {
-
-  final List<OffCampus> _store;
-
-  OffList(this._store);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          
-          children: _buildOffList()
-          
-        );
-  }
-
-  List<_OffListItem> _buildOffList() {
-    return _store.map((store) => _OffListItem(store))
-                    .toList();
-  }
-
-}
-
-class _OffListItem extends ListTile {
-
-  _OffListItem(OffCampus store) :
-    super(
-      title : Text(store.storeName),
-      subtitle: Text(store.address),
-      trailing: Text(store.phone),
-      leading: CircleAvatar(
-        child: Text(store.storeName[0])
-      )
-    );
-
 }
