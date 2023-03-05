8e2e6b9a1955f8c9d0d530c394036cf83a6df3c3
Ryan Pollard <71097838+ryanpoll@users.noreply.github.com>
2023-03-05 12:20:08 -0500

    Update map_markers.dart

--- lib/map_markers.dart
+++ lib/map_markers.dart
@@ -1,232 +1,176 @@
 import 'package:flutter/scheduler.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class SchoolMarker {
-  final String markerId;
-  final LatLng position;
-  final String infoWindow;
-
-  const SchoolMarker(
-      {required this.markerId,
-      required this.position,
-      required this.infoWindow});
-
-  static const schoolMarkers = <SchoolMarker>[
-    SchoolMarker(
-        markerId: 'buley',
-        position: LatLng(41.33305, -72.94781),
-        infoWindow: 'Buley Library'),
-    SchoolMarker(
-        markerId: 'jennings',
-        position: LatLng(41.33419, -72.94519),
-        infoWindow: 'Jennings Hall'),
-    SchoolMarker(
-        markerId: 'morrill',
-        position: LatLng(41.33372, -72.94585),
-        infoWindow: 'Morrill Hall'),
-    SchoolMarker(
-        markerId: 'engleman',
-        position: LatLng(41.33263, -72.94656),
-        infoWindow: 'Engleman Hall'),
-    SchoolMarker(
-        markerId: 'temp',
-        position: LatLng(41.33176, -72.94506),
-        infoWindow: 'Temporary Building #8'),
-    SchoolMarker(
-        markerId: 'school of business',
-        position: LatLng(41.33161, -72.94553),
-        infoWindow: 'School of Business'),
-    SchoolMarker(
-        markerId: 'lyman',
-        position: LatLng(41.33151, -72.94683),
-        infoWindow: 'Lyman Center'),
-    SchoolMarker(
-        markerId: 'earl',
-        position: LatLng(41.33196, -72.94803),
-        infoWindow: 'Earl Hall'),
-    SchoolMarker(
-        markerId: 'science building',
-        position: LatLng(41.33477, -72.94562),
-        infoWindow: 'Academic Science and Laboratory Building'),
-    SchoolMarker(
-        markerId: 'nursing building',
-        position: LatLng(41.33592, -72.94198),
-        infoWindow: 'Nursing Building'),
-    SchoolMarker(
-        markerId: 'facilities',
-        position: LatLng(41.33647, -72.94196),
-        infoWindow: 'Facilities Operations Building'),
-    SchoolMarker(
-        markerId: 'schwartz hall',
-        position: LatLng(41.33144, -72.95015),
-        infoWindow: 'Schwartz Hall'),
-    SchoolMarker(
-        markerId: 'wintergreen building',
-        position: LatLng(41.33462, -72.95085),
-        infoWindow: 'Wintergreen Building'),
-  ];
-}
-
-///////////////////////////////////////////////////////////////////////////////////
-
-class ParkingMarker {
-  final String markerId;
-  final LatLng position;
-  final String infoWindow;
-
-  const ParkingMarker(
-      {required this.markerId,
-      required this.position,
-      required this.infoWindow});
-
-  static const parkingMarkers = <ParkingMarker>[
-    ParkingMarker(
-        markerId: 'fitch street garage',
-        position: LatLng(41.33573, -72.94296),
-        infoWindow: 'Fitch Street Garage'),
-    ParkingMarker(
-        markerId: 'west campus garage',
-        position: LatLng(41.33312, -72.95375),
-        infoWindow: 'West Campus Garage'),
-    ParkingMarker(
-        markerId: 'wintergreen garage',
-        position: LatLng(41.33561, -72.95307),
-        infoWindow: 'Wintergreen Garage'),
-  ];
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-class ShuttleMarker {
-  final String markerId;
-  final LatLng position;
-
-  const ShuttleMarker({
-    required this.markerId,
-    required this.position,
-  });
-
-  static const shuttleMarkers = <ShuttleMarker>[
-    ShuttleMarker(
-      markerId: 's1',
-      position: LatLng(41.33487, -72.95224),
-    ),
-    ShuttleMarker(
-      markerId: 's2',
-      position: LatLng(41.33345, -72.95326),
-    ),
-    ShuttleMarker(
-      markerId: 's3',
-      position: LatLng(41.33411, -72.94646),
-    ),
-    ShuttleMarker(
-      markerId: 's4',
-      position: LatLng(41.33148, -72.95392),
-    ),
-    ShuttleMarker(
-      markerId: 's5',
-      position: LatLng(41.33003, -72.95339),
-    ),
-    ShuttleMarker(
-      markerId: 's6',
-      position: LatLng(41.33130, -72.94555),
-    ),
-    ShuttleMarker(
-      markerId: 's7',
-      position: LatLng(41.33516, -72.94328),
-    ),
-    ShuttleMarker(
-      markerId: 's8',
-      position: LatLng(41.33581, -72.94230),
-    ),
-    ShuttleMarker(
-      markerId: 's9',
-      position: LatLng(41.33718, -72.94690),
-    ),
-  ];
-}
-
-/////////////////////////////////////////////////////////////////////////
-
-class BusMarker {
-  final String markerId;
-  final LatLng position;
-
-  const BusMarker({
-    required this.markerId,
-    required this.position,
-  });
-
-  static const busMarkers = <BusMarker>[
-    BusMarker(
-      markerId: 'b1',
-      position: LatLng(41.33213, -72.94930),
-    ),
-    BusMarker(
-      markerId: 'b2',
-      position: LatLng(41.33241, -72.94950),
-    ),
-    BusMarker(
-      markerId: 'b3',
-      position: LatLng(
-        41.33430,
-        -72.95209,
-      ),
-    )
-  ];
-}
-
-/////////////////////////////////////////////////////////////////////////////
-
-class SportsMarker {
-  final String markerId;
+class MapMarker {
+  final MarkerId markerId;
   final LatLng position;
   final InfoWindow infoWindow;
 
-  const SportsMarker(
+  const MapMarker(
       {required this.markerId,
       required this.position,
       required this.infoWindow});
 
-  static const sportsMarkers = <SportsMarker>[
-    SportsMarker(
-        markerId: 'jess dow field',
+// markers for academic buildings
+  static const schoolMarkers = <MapMarker>[
+    MapMarker(
+        markerId: MarkerId('buley'),
+        position: LatLng(41.33305, -72.94781),
+        infoWindow: InfoWindow(title: 'Buley Library')),
+    MapMarker(
+        markerId: MarkerId('jennings'),
+        position: LatLng(41.33419, -72.94519),
+        infoWindow: InfoWindow(title: 'Jennings Hall')),
+    MapMarker(
+        markerId: MarkerId('morrill'),
+        position: LatLng(41.33372, -72.94585),
+        infoWindow: InfoWindow(title: 'Morrill Hall')),
+    MapMarker(
+        markerId: MarkerId('engleman'),
+        position: LatLng(41.33263, -72.94656),
+        infoWindow: InfoWindow(title: 'Engleman Hall')),
+    MapMarker(
+        markerId: MarkerId('temp'),
+        position: LatLng(41.33176, -72.94506),
+        infoWindow: InfoWindow(title: 'Temporary Building #8')),
+    MapMarker(
+        markerId: MarkerId('school of business'),
+        position: LatLng(41.33161, -72.94553),
+        infoWindow: InfoWindow(title: 'School of Business')),
+    MapMarker(
+        markerId: MarkerId('lyman'),
+        position: LatLng(41.33151, -72.94683),
+        infoWindow: InfoWindow(title: 'Lyman Center')),
+    MapMarker(
+        markerId: MarkerId('earl'),
+        position: LatLng(41.33196, -72.94803),
+        infoWindow: InfoWindow(title: 'Earl Hall')),
+    MapMarker(
+        markerId: MarkerId('science building'),
+        position: LatLng(41.33477, -72.94562),
+        infoWindow:
+            InfoWindow(title: 'Academic Science and Laboratory Building')),
+    MapMarker(
+        markerId: MarkerId('nursing building'),
+        position: LatLng(41.33592, -72.94198),
+        infoWindow: InfoWindow(title: 'Nursing Building')),
+    MapMarker(
+        markerId: MarkerId('facilities'),
+        position: LatLng(41.33647, -72.94196),
+        infoWindow: InfoWindow(title: 'Facilities Operations Building')),
+    MapMarker(
+        markerId: MarkerId('schwartz hall'),
+        position: LatLng(41.33144, -72.95015),
+        infoWindow: InfoWindow(title: 'Schwartz Hall')),
+    MapMarker(
+        markerId: MarkerId('wintergreen building'),
+        position: LatLng(41.33462, -72.95085),
+        infoWindow: InfoWindow(title: 'Wintergreen Building')),
+  ];
+
+// markers for parking garages
+  static const parkingMarkers = <MapMarker>[
+    MapMarker(
+        markerId: MarkerId('fitch street garage'),
+        position: LatLng(41.33573, -72.94296),
+        infoWindow: InfoWindow(title: 'Fitch Street Garage')),
+    MapMarker(
+        markerId: MarkerId('west campus garage'),
+        position: LatLng(41.33312, -72.95375),
+        infoWindow: InfoWindow(title: 'West Campus Garage')),
+    MapMarker(
+        markerId: MarkerId('wintergreen garage'),
+        position: LatLng(41.33561, -72.95307),
+        infoWindow: InfoWindow(title: 'Wintergreen Garage')),
+  ];
+
+// markers for Shuttle Buss
+  static const shuttleMarkers = <MapMarker>[
+    MapMarker(
+        markerId: MarkerId('s1'),
+        position: LatLng(41.33487, -72.95224),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s2'),
+        position: LatLng(41.33345, -72.95326),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s3'),
+        position: LatLng(41.33411, -72.94646),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s4'),
+        position: LatLng(41.33148, -72.95392),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s5'),
+        position: LatLng(41.33003, -72.95339),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s6'),
+        position: LatLng(41.33130, -72.94555),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s7'),
+        position: LatLng(41.33516, -72.94328),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s8'),
+        position: LatLng(41.33581, -72.94230),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+    MapMarker(
+        markerId: MarkerId('s9'),
+        position: LatLng(41.33718, -72.94690),
+        infoWindow: InfoWindow(title: 'Shuttle Bus')),
+  ];
+
+// markers for bus stops
+  static const busMarkers = <MapMarker>[
+    MapMarker(
+        markerId: MarkerId('b1'),
+        position: LatLng(41.33213, -72.94930),
+        infoWindow: InfoWindow(title: 'Municipal Bus')),
+    MapMarker(
+        markerId: MarkerId('b2'),
+        position: LatLng(41.33241, -72.94950),
+        infoWindow: InfoWindow(title: 'Municipal Bus')),
+    MapMarker(
+        markerId: MarkerId('b3'),
+        position: LatLng(
+          41.33430,
+          -72.95209,
+        ),
+        infoWindow: InfoWindow(title: 'Municipal Bus'))
+  ];
+
+// markers for sports facilities
+  static const sportsMarkers = <MapMarker>[
+    MapMarker(
+        markerId: MarkerId('jess dow field'),
         position: LatLng(41.33661, -72.95001),
         infoWindow: InfoWindow(
             title: 'Jess Dow Field',
             snippet: "Football, Soccer, Field Hockey, Lacrosse")),
-    SportsMarker(
-        markerId: 'pelz field',
+    MapMarker(
+        markerId: MarkerId('pelz field'),
         position: LatLng(41.33417, -72.94329),
         infoWindow: InfoWindow(title: 'Pelz Field', snippet: "Baseball Field")),
-    SportsMarker(
-        markerId: 'ballpark',
+    MapMarker(
+        markerId: MarkerId('ballpark'),
         position: LatLng(41.33259, -72.94322),
         infoWindow: InfoWindow(
             title: 'The Ballpark at SCSU', snippet: "Baseball Field")),
   ];
-}
 
-//////////////////////////////////////////////////
-
-class DiningMarker {
-  final String markerId;
-  final LatLng position;
-  final String infoWindow;
-
-  const DiningMarker(
-      {required this.markerId,
-      required this.position,
-      required this.infoWindow});
-
-  static const diningMarkers = <DiningMarker>[
-    DiningMarker(
-        markerId: 'conn hall',
+// markers for dining spots
+  static const diningMarkers = <MapMarker>[
+    MapMarker(
+        markerId: MarkerId('conn hall'),
         position: LatLng(41.33256, -72.95024),
-        infoWindow: 'Connecticut Hall'),
-    DiningMarker(
-        markerId: 'adanti',
+        infoWindow: InfoWindow(title: 'Connecticut Hall')),
+    MapMarker(
+        markerId: MarkerId('adanti'),
         position: LatLng(41.33182, -72.94882),
-        infoWindow: 'Adanti Student Center'),
+        infoWindow: InfoWindow(title: 'Adanti Student Center')),
   ];
 }
