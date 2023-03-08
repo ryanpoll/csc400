c776bee46d55276eec52e97a43f1349b6ce10dff
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-03-07 19:25:21 -0500

    Update other.dart

--- lib/other.dart
+++ lib/other.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+import './upcomingEvents.dart';
+import './academicDates.dart';
+
 
 class Other extends StatelessWidget {
+   static const String routeName = '/other';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(221, 2, 18, 200),
         title: const Text('Other Resources'),
       ),
       body: Center(
-        child: ElevatedButton(
-            child: const Text('Back To Home Screen'),
-            // brings back to home screen
-            onPressed: () => Navigator.pop(context)),
-      ),
-    );
+        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+            ElevatedButton(
+                child: const Text('Housing'),
+                style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(220, 70),
+                      backgroundColor: Color.fromARGB(221, 2, 18, 200)),
+                // brings back to home screen
+                onPressed: () => Navigator.pop(context)),
+                ElevatedButton(
+                child: const Text('Upcoming Events'),
+                style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(220, 70),
+                      backgroundColor: Color.fromARGB(221, 2, 18, 200)),
+                // brings back to home screen
+                onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (BuildContext context)=>  TableBasicsExample()))),
+            ElevatedButton(child: const Text('Academic Dates'),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(220, 70),
+                      backgroundColor: Color.fromARGB(221, 2, 18, 200)),
+                  onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context)=> NotesScreen()))),
+              ]
+              ),
+      
+    ));
   }
 }
