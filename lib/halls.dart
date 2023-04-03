073404e0843880740a0454ca64e5454599695f2a
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-03 19:55:07 -0400

    Create halls.dart

---
+++ lib/halls.dart
@@ -0,0 +1,102 @@
+class ResHalls{
+  String name;
+  String address;
+  String phone;
+  String websiteUrl;
+  String imageUrl;
+  String description;
+
+ ResHalls(
+      { required this.name,
+      required this.address,
+      required this.phone,
+      required this.websiteUrl,
+      required this.imageUrl,
+      required this.description});
+
+}
+
+
+List<ResHalls> resList = <ResHalls>[
+    ResHalls(
+      name: 'Brownell Hall',
+      address:'114 Farnham Ave, New Haven, CT 06515',
+      phone: '(203) 392-6374',
+      websiteUrl: 'Link to visual tour: https://visual.southernct.edu/places/brownell',
+      imageUrl: 'https://cdn.mydorm.com/images/e646f2ce-f614-4c52-9aaa-216bbe05b462.jpg',
+      description:'Brownell Hall offers suite-style living for four or six students in double-occupancy bedrooms. Each suite includes a bathroom, living room, and pantry. Some suites also include a kitchen, and all rooms are air-conditioned. Residents have access to open study spaces, a TV lounge/video game room and kitchen area on the third floor; pool and foosball tables; and laundry on the first floor. Brownell residents who live in a suite without a kitchen are required to purchase a full meal plan. Students living in on-campus apartments with a kitchen are required to purchase a per-semester block meal plan.'
+    ),
+    ResHalls(
+      name: 'Chase Hall',
+      address:'174 Farnham Ave, New Haven, CT 06515',
+      phone: '(203) 392-6363',
+      websiteUrl: 'Link to visual tour: https://visual.southernct.edu/places/chase',
+      imageUrl: 'https://cdn.mydorm.com/images/9b0c99c0-0a42-4845-b1a5-4302878c5005.jpg',
+      description: "Chase Hall is a traditional on-campus residence hall with double-occupancy rooms and community bathrooms. Residents have access to on-floor laundry, lounges, and kitchen areas. Recreation equipment and lounge space are also available on the first floor and in the building's basement. There are also a limited amount of single-occupancy rooms."
+    ),
+    ResHalls(
+      name: 'Farnham Hall',
+      address:'166 Farnham Ave, New Haven, CT 06515',
+      phone: '(203) 392-5436',
+      websiteUrl: 'Link to visual tour: https://visual.southernct.edu/places/farnham',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Farnham_Hall.jpg/1200px-Farnham_Hall.jpg',
+      description: "Farnham Hall is a traditional on-campus residence hall with double-occupancy rooms and community bathrooms. Residents have access to on-floor laundry, lounges, and kitchen areas; a video game room on the first floor; and additional recreation equipment in the building's spacious lobby lounge. Farnham Hall is also home to the Farnham Programming Space, Residence Life's evening and weekend programming venue open to all Southern students. There are also a limited amount of single-occupancy rooms."
+    ),
+    ResHalls(
+      name: 'Hickerson Hall',
+      address:'100 Wintergreen Ave, New Haven, CT 06515',
+      phone: '(203) 392-6367',
+      websiteUrl: 'Link to visual tour: https://visual.southernct.edu/places/hickerson',
+      imageUrl: 'https://cdn.mydorm.com/images/3bf254bb-4369-4c60-b6de-339f058f92c2.jpg',
+      description: "Hickerson Hall is a traditional on-campus residence hall with double-occupancy rooms and community bathrooms. Residents have access to on-floor laundry, lounges, and kitchen areas. Recreation equipment and lounge space are also available on the first floor and in the building's basement. There are also a limited amount of single-occupancy rooms."
+    ),
+    ResHalls(
+      name: 'Neff Hall',
+      address:'112 Wintergreen Ave, New Haven, CT 06515',
+      phone: '(203) 392-6356',
+      websiteUrl: 'Link to visual tour: http://visual.southernct.edu/places/neff',
+      imageUrl: 'https://thesouthernnews.files.wordpress.com/2019/10/annotation-2019-10-02-032425.png?w=640',
+      description: "Neff Hall is a traditional on-campus residence hall that offers single-occupancy rooms and community bathrooms. Residents have access to on-floor laundry, lounges with large study tables, and kitchen areas. Recreation equipment and lounge space are also available on the first floor and in the building's basement."
+    ),
+    ResHalls(
+      name: 'North Campus Residence Complex',
+      address:'180 Pine Rock Ave, Hamden, CT 06514',
+      phone: '(203) 392-6379',
+      websiteUrl: 'Link to visual tour: http://visual.southernct.edu/places/north-campus',
+      imageUrl: 'https://www.southernct.edu/sites/default/files/inline-images/north-campus-residence-complex-kitchen.jpg',
+      description: 'The North Campus Residence Complex includes both the large Midrise building and the individual Townhouse units. Upper-class students can request four-person accommodations in either the Midrise or the Townhouses, both of which feature carpeting, air conditioning, storage spaces, and full kitchens. A community quiet study area and the North Campus computer lab are located on the first floor of the Midrise; both spaces are accessible to Midrise and Townhouse residents. Laundry facilities are located on each floor in the Midrise as well, and the first- floor Marketplace serves as a convenience store for all Southern students. Students living in on-campus apartments with a kitchen are required to purchase a per-semester block meal plan.'
+    ),
+    ResHalls(
+      name: 'Schwartz Hall',
+      address:'320 Fitch Street, New Haven, CT 06515',
+      phone: '(203) 392-6120',
+      websiteUrl: 'Link to visual tour: http://visual.southernct.edu/places/schwartz',
+      imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipOgoyx2s1jSjNa0iXAenp-FEfcKs09EYqwW8jh9=w1080-h608-p-no-v0',
+      description: "Schwartz Hall offers housing accommodations for upperclassmen in two- or four-person apartments. All apartments are furnished and feature carpeting, air conditioning, storage space, and full kitchens. A community lounge is available on the first floor, and several campus resources are located within the building: Office of Residence Life; Violence Prevention, Victim Advocacy and Support Center (VPAS); and Wellness Center. A full laundry room is available for residents' use in the basement. Students living in on-campus apartments with a kitchen are required to purchase a per-semester block meal plan."
+    ),
+    ResHalls(
+      name: 'West Campus Residence Complex',
+      address:'116 Wintergreen Ave, New Haven, CT 06515',
+      phone: '203-691-9064',
+      websiteUrl: 'Link to visual tour: https://visual.southernct.edu/places/west',
+      imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipOgoyx2s1jSjNa0iXAenp-FEfcKs09EYqwW8jh9=w1080-h608-p-no-v0',
+      description: 'The West Campus Residence Complex offers four-person, single bedroom suites as well as standard double-occupancy rooms with community bathrooms. All rooms are air conditioned. Residents have access to social and quiet study lounges, a computer lab, a video game room, a large laundry facility and kitchen area on the first floor, and recreation equipment.'
+    ),
+    ResHalls(
+      name: 'Wilkinson Hall',
+      address:'158 Farnham Ave, New Haven, CT 06515',
+      phone: '203-691-9064',
+      websiteUrl: 'Link to visual tour: http://visual.southernct.edu/places/wilkinson',
+      imageUrl: 'https://cdn.mydorm.com/images/406e4162-de92-41d2-8cd4-66780e4db0ae.jpg',
+      description: 'Wilkinson Hall is a traditional on-campus residence hall featuring double-occupancy rooms with community bathrooms. Residents have access to on-floor laundry and lounges, a kitchen area, and recreation equipment in the building. The spacious lobby is the frequent site of all-hall programming. There are also a limited amount of single-occupancy rooms.'
+    ),
+    ResHalls(
+      name: 'North Campus Townhouses',
+      address:'180 Pine Rock Ave, New Haven, CT 06515',
+      phone: '203-691-9064',
+      websiteUrl: 'Link to visual tour: http://visual.southernct.edu/places/north-campus-townhouses',
+      imageUrl: 'https://www.southernct.edu/sites/default/files/inline-images/north-campus-townhouse-living-room.jpg',
+      description: 'The North Campus Residence Complex includes both the large Midrise building and the individual Townhouse units. Upper-class students can request four-person accommodations in either the Midrise or the Townhouses, both of which feature carpeting, air conditioning, storage spaces, and full kitchens. A community quiet study area and the North Campus computer lab are located on the first floor of the Midrise; both spaces are accessible to Midrise and Townhouse residents. Laundry facilities are located on each floor in the Midrise as well, and the first- floor Marketplace serves as a convenience store for all Southern students. Students living in on-campus apartments with a kitchen are required to purchase a per-semester block meal plan. There are also a limited amount of single-occupancy bedrooms in both the Midrise and Townhouse units.'
+    ),
+    
+  ];
