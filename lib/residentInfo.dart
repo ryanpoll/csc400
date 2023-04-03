61fba0aec4eb47f539ac2ca0cbde268a72bb66f7
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-03 19:56:25 -0400

    Create residentInfo.dart

---
+++ lib/residentInfo.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/res_details.dart';
+import 'styles.dart';
+import 'halls.dart';
+import 'res_details.dart';
+
+
+
+class ResidentInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(221, 2, 18, 200),        
+          centerTitle: true,
+          title: Text('Residence Halls', style: ThemeText.otherAppBars),
+          
+        ),
+        body: ListView.builder(
+            itemCount: resList.length,
+            itemBuilder: (context, index) {
+              ResHalls hall = resList[index];
+              return Card(
+                child: ListTile(
+                  title: Text(hall.name),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ResidentDetails(hall)));
+                  },
+                ),
+              );
+            }));
+  }
+  
+}
+
