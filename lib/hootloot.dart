f31cdaaa0aa161a2e2e1ab310457b174cea0a02d
Ryan Pollard <71097838+ryanpoll@users.noreply.github.com>
2023-04-03 19:45:48 -0400

    Update hootloot.dart

--- lib/hootloot.dart
+++ lib/hootloot.dart
@@ -1,42 +1,91 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/hootLootOn.dart';
 import 'package:flutter_application_1/hootLootOff.dart';
+import 'styles.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HootLoot extends StatelessWidget {
- @override
+  const HootLoot({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 0, 21, 255),
-          title: const Text(
-            'HootLoot',
-          ),
+          backgroundColor: ThemeText.mainColor,
+          title: const Text('Hoot Loot', style: ThemeText.otherAppBars),
         ),
         body: Center(
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Image.asset('images/hootLootCard.png',
+                    width: 350.0, height: 220.0),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 15, 20, 30),
+                  child: Text(
+                      "The SCSU Hoot Loot card provides access to residence halls, meal plans, computer labs, and also functions as your library card. Below you can find On Campus and Off Campus locations where you can use your Hoot Loot card.",
+                      style: GoogleFonts.lato(
+                          fontSize: 19, wordSpacing: 3, color: Colors.black)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  child: ElevatedButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'On Campus',
+                          style: ThemeText.otherButtonText,
+                          textAlign: TextAlign.center,
+                        ),
+                        Image.asset('images/onCampus.png',
+                            width: 50.0, height: 50.0)
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(220, 90),
+                        shadowColor: ThemeText.mainColor,
+                        elevation: 10.0,
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25))),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => HootLootOn()));
+                    },
+                  ),
+                ),
                 ElevatedButton(
-                  child: const Text('On Campus'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Off Campus',
+                        style: ThemeText.otherButtonText,
+                        textAlign: TextAlign.center,
+                      ),
+                      Image.asset('images/gps.png', width: 50.0, height: 50.0)
+                    ],
+                  ),
                   style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(220, 70),
-                      backgroundColor: Color.fromARGB(255, 0, 21, 255)),
+                      fixedSize: const Size(220, 90),
+                      shadowColor: ThemeText.mainColor,
+                      elevation: 10.0,
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25))),
                   onPressed: () {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (BuildContext context) => HootLootOn()));
-                  },
-                ),
-                ElevatedButton(
-                  child: const Text('Off Campus'),
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(220, 70),
-                      backgroundColor: Color.fromARGB(255, 0, 21, 255)),
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                            builder: (BuildContext context) => HootLoot()));
+                            builder: (BuildContext context) => HootLootOff()));
                   },
                 ),
               ]),
