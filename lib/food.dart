5260cecd9299b268ac8608c0f93fba129b4d268b
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-11 18:07:01 -0400

    Update food.dart

--- lib/food.dart
+++ lib/food.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'styles.dart';
+import 'foodPlacesInfo.dart';
+import 'foodDetails.dart';
+
 
 class Food extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Food'),
+        backgroundColor: Color.fromARGB(221, 2, 18, 200),
+        title: const Text('Food', style: ThemeText.otherAppBars),
       ),
-      body: Center(
-        child: ElevatedButton(
-            child: const Text('Back To Home Screen'),
-            // brings back to home screen
-            onPressed: () => Navigator.pop(context)),
-      ),
+      body: ListView.builder(
+            itemCount: foodList.length,
+            itemBuilder: (context, index) {
+              FoodLocations food = foodList[index];
+              return Card(
+                child: ListTile(
+                  title: Text(food.name),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => FoodDetails(food)));
+                  },
+                ),
+              );
+            }),
     );
   }
 }
