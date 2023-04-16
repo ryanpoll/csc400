490a1894a9314b2f7ee9f6ad9dbf723d5339df63
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-16 17:12:26 -0400

    Update off_campus.dart

--- lib/off_campus.dart
+++ lib/off_campus.dart
@@ -1,6 +1,8 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class OffCampus {
   String name;
-  String address;
+  final LatLng position;
   String phone;
   String websiteUrl;
   String hours;
@@ -9,19 +11,20 @@ class OffCampus {
 
  OffCampus(
       { required this.name,
-      required this.address,
+      required this.position,
       required this.phone,
       required this.websiteUrl,
       required this.hours,
       required this.imageUrl,
       required this.description});
+
 }
 
 
 List<OffCampus> storeList = <OffCampus>[
     OffCampus(
       name: 'Acropolis Diner',
-      address:'1864 Dixwell Ave, Hamden, CT',
+      position: LatLng(41.36279799134644, -72.9241940779119),
       phone: '203-288-0400',
       websiteUrl: 'http://acropolisdinerct.com/',
       hours: ' Monday-Sunday: 6AM-12AM',
@@ -30,7 +33,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'Ali Babas Kitchen',
-      address:'986 Dixwell Ave, Hamden, CT',
+      position:LatLng(41.34049089033041, -72.935556343804629),
       phone: '203-535-1164',
       websiteUrl: 'https://www.alibabaskitchen.com/',
       hours:'Sunday & Monday-Thursday: 11AM-10PM \n Saturday & Friday: 11AM-11PM',
@@ -39,25 +42,17 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'Blake Street Convenience',
-      address:'295 Blake Street, New Haven, CT',
+      position: LatLng(41.328065870507714, -72.95311087264024),
       phone: '203-691-9064',
       websiteUrl: 'https://blakeconvenience.com/',
       hours:'Monday-Sunday: 7AM-12AM',
       imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipOgoyx2s1jSjNa0iXAenp-FEfcKs09EYqwW8jh9=w1080-h608-p-no-v0',
       description: 'Convenience store and smoke shop offereing beverages, snack, and more! '
     ),
-    OffCampus(
-      name: "Brother Jimmy's BBQ",
-      address:'196 Crown Street, New Haven, CT',
-      phone: '203-535-0843',
-      websiteUrl: 'https://brotherjimmys.com/new_haven/',
-      hours:'Monday - Thursday 11:30am - 10pm \n Friday 11:30am - 11pm \n Saturday 11:30am - 11pm \n Sunday 11am - 10pm',
-      imageUrl: 'https://brotherjimmys.com/static/images/logo.png',
-      description: "Brother Jimmy's is a full service, high energy sports bar and restaurant specializing in the finest of Southern BBQ for over 25 Years."
-),
+    
     OffCampus(
       name: 'China Wok',
-      address:'1245 Dixwell Ave, Hamden, CT',
+      position: LatLng(41.348178135290006, -72.93151550147499),
       phone: '203-288-9403',
       websiteUrl: 'https://www.chinawokhamden.com/',
       hours:'Monday-Thursday: 11:00AM-10:30PM \n Friday & Saturday: 11:00AM-11:00PM \n Sunday: 11:30AM-10:30PM',
@@ -66,7 +61,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'Cilantro Mexican Grill',
-      address:'1158 Whalley Ave, New Haven, CT',
+      position: LatLng(41.32928970688495, -72.9687599879811),
       phone: '203-389-1212',
       websiteUrl: 'http://cilantrofreshmexicangrill.com/',
       hours:'Sunday & Monday-Thursday: 11:30AM-9:00PM \n Friday & Saturday: 11:30AM-10:00PM',
@@ -75,7 +70,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'CVS/Pharmacy',
-      address:'1150 Whalley Ave, New Haven, CT',
+      position: LatLng(41.32937584901871, -72.96969346450801),
       phone: '203-389-4714',
       websiteUrl: 'https://www.cvs.com/',
       hours:'Monday-Friday: 8:00AM-10:00PM \n Saturday & Sunday: 8:00AM-9:00PM',
@@ -84,7 +79,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'CVS/Pharmacy',
-      address:'2045 Dixwell Ave, Hamden, CT',
+      position: LatLng(41.367499219927296, -72.91964294380422),
       phone: '203-389-4714',
       websiteUrl: 'https://www.cvs.com/',
       hours:'Monday-Friday: 8:00AM-10:00PM \n Saturday & Sunday: 8:00AM-10:00PM',
@@ -93,7 +88,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'Dayton Street Apizza',
-      address:'60 Dayton Street, New Haven, CT',
+      position: LatLng(41.329185237413014, -72.96865709962873),
       phone: '203-389-2454',
       websiteUrl: 'http://daytonstreetapizza.com/',
       hours:'Sunday & Monday-Thursday: 11:30AM-9:00PM \n Friday & Saturday: 11:30AM-10:00PM',
@@ -102,7 +97,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: "Domino's Pizza",
-      address:'357 Whalley Ave, New Haven, CT',
+      position: LatLng(41.31869552114516, -72.94906817264041),
       phone: '203-624-3317',
       websiteUrl: 'Sunday & Monday-Thursday: 10:30AM-12:00AM \n Friday & Saturday: 10:30AM-1:00AM',
       hours:'',
@@ -112,7 +107,7 @@ List<OffCampus> storeList = <OffCampus>[
    
     OffCampus(
       name: 'Ixtapa Grill Mexican Restaurant',
-      address:'2547 Whitney Ave Hamden, CT',
+      position: LatLng(41.38933591466846, -72.8992017438038),
       phone: '203-230-2586',
       websiteUrl: 'https://ixtapagrille.net/',
       hours:'Sunday & Monday-Thursday: 10:00AM-9:00PM \n Friday & Saturday: 11:00AM-10:00PM',
@@ -121,7 +116,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'Meineke Car Care Center',
-      address:'1175 Whalley Ave, New Haven, CT',
+      position: LatLng(41.329893304869735, -72.96906278798102),
       phone: '203-397-2353',
       websiteUrl: 'http://www.meineke.com/',
       hours:'Monday-Saturday: 7:30AM-6:00PM \n Sunday: Closed',
@@ -130,7 +125,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
     OffCampus(
       name: 'More Than Pizza',
-      address:'283 Blake Street, New Haven, CT',
+      position: LatLng(41.32746089934973, -72.95298904380503),
       phone: '203-389-2222',
       websiteUrl: 'https://www.morethanpizzact.com/',
       hours:'Monday-Wednesday: 11:00AM-2:00AM \n Thursday-Saturday:11:00AM-3:00AM \n Sunday: 12:00PM-2:00AM',
@@ -140,7 +135,7 @@ List<OffCampus> storeList = <OffCampus>[
   
     OffCampus(
       name: 'Pizza Heaven II',
-      address:'410 Blake Street, New Haven, CT',
+      position: LatLng(41.32874456058088, -72.95659658615234),
       phone: '203-764-2426',
       websiteUrl: 'https://www.pizzaheaven2.com/',
       hours:'Monday-Wednesday: 11:00AM-1:30AM \n Thursday-Saturday: 11:00AM-2:30AM \n Sunday: 12:00PM-1:30AM',
@@ -148,62 +143,54 @@ List<OffCampus> storeList = <OffCampus>[
       description: 'Pizza Heaven 2 serves high quality food that is prepared by using fresh ingredients.'
     ),
     OffCampus(
-      name: 'Rite Aid #00441',
-      address:'66 Church Street, New Haven, CT',
+      name: 'Rite Aid',
+      position: LatLng(41.305245110634864, -72.92681989962924),
       phone: '203-777-7248',
       websiteUrl: 'https://www.riteaid.com/',
-      hours:'Monday-Friday: 8:00AM-10:00PM \n Saturday & Sunday: 9:00AM-9:00PM',
+      hours:'66 Church Street \n New Haven, CT \n Monday-Friday: 8:00AM-10:00PM \n Saturday & Sunday: 9:00AM-9:00PM',
       imageUrl: 'https://s3-media0.fl.yelpcdn.com/bphoto/qjYa4XXqwYlvRCQPbtMgqw/348s.jpg',
       description: 'Rite Aid pharmacy offers products and services to help you lead a healthy, happy life. Visit our online pharmacy, shop now, or find a store near you.'
     ),
+    
     OffCampus(
-      name: 'Rite Aid #01033',
-      address:'85 Middletown Ave, North Haven, CT',
-      phone: '203-865-3179',
-      websiteUrl: 'https://www.riteaid.com/',
-      hours:'Monday-Friday: 8:00AM-9:00PM \n Saturday: 8:00AM-7:00PM \n Sunday: 8:00AM-6:00PM',
-      imageUrl: 'https://images1.loopnet.com/i2/GzaStBVrmtgYMQWXdTp2-He77ujnmgCShXvXvHcK6eM/112/image.jpg',
-      description: 'Rite Aid pharmacy offers products and services to help you lead a healthy, happy life. Visit our online pharmacy, shop now, or find a store near you.'
-    ),
-    OffCampus(
-      name: 'Rite Aid #01839',
-      address:'70 Elm Street, West Haven, CT',
+      name: 'Rite Aid',
+      position: LatLng(41.27875000123377, -72.9421954321582),
       phone: '203-933-5260',
       websiteUrl: 'https://www.riteaid.com/',
-      hours:'Monday-Saturday: 8:00AM-10:00PM \n 8:00AM-9:00PM',
+      hours:' 70 Elm Street \n West Haven,CT \n Monday-Saturday: 8:00AM-10:00PM \n 8:00AM-9:00PM',
       imageUrl: 'https://dynl.mktgcdn.com/p/o6XUI2MYWN1iHzPAh8O44RQ6b10d21jx_C9O4u2J6Bw/600x338.jpg',
       description: 'Rite Aid pharmacy offers products and services to help you lead a healthy, happy life. Visit our online pharmacy, shop now, or find a store near you.'
     ),
     OffCampus(
-      name: 'Rite Aid #01882',
-      address:'325 Ferry Street, New Haven, CT',
+      name: 'Rite Aid',
+      position: LatLng(41.30895463387835, -72.89478664380532),
       phone: '203-777-0695',
       websiteUrl: 'https://www.riteaid.com/',
-      hours:'Monday-Sunday: 8:00AM-9:00PM',
+      hours:' 325 Ferry Street \n New Haven, CT \n Monday-Sunday: 8:00AM-9:00PM',
       imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipNogrQDECy0Pyw7pgBfMCUFLvqYQOh_-DIFFykk=s1360-w1360-h1020',
       description: 'Rite Aid pharmacy offers products and services to help you lead a healthy, happy life. Visit our online pharmacy, shop now, or find a store near you.'
     ),
     OffCampus(
-      name: 'Rite Aid #04778',
-      address:'588 Main Street, East Haven, CT',
+      name: 'Rite Aid',
+      position: LatLng(41.28332603756484, -72.882941730311521),
       phone: '203-469-7648',
       websiteUrl: 'https://www.riteaid.com/',
-      hours:'Monday-Saturday: 8:00AM-10:00PM \n Sunday: 8:00AM-9:00PM',
+      hours:'588 Main Street \n East Haven, CT \n Monday-Saturday: 8:00AM-10:00PM \n Sunday: 8:00AM-9:00PM',
       imageUrl: 'https://dynl.mktgcdn.com/p/DgqR_iW-Ax0tsHG_6xarK2ahuQRY4nyUkUcIuClGCmk/400x400.jpg',
       description: 'Rite Aid pharmacy offers products and services to help you lead a healthy, happy life. Visit our online pharmacy, shop now, or find a store near you.'
     ),
     OffCampus(
-      name: 'Rite Aid #10375',
-      address:'2175 Dixwell Ave, Hamden, CT',
+      name: 'Rite Aid',
+      position: LatLng(41.37010612519117, -72.9181065168155),
       phone: '203-288-3895',
       websiteUrl: 'https://www.riteaid.com/',
-      hours:'Monday-Saturday: 8:00AM-9:00PM \n 8:00AM-6:00PM',
+      hours:'2175 Dixwell Ave \n Hamden, CT \n Monday-Saturday: 8:00AM-9:00PM \n 8:00AM-6:00PM',
       imageUrl: 'https://dynl.mktgcdn.com/p/DgqR_iW-Ax0tsHG_6xarK2ahuQRY4nyUkUcIuClGCmk/400x400.jpg',
       description: 'Rite Aid pharmacy offers products and services to help you lead a healthy, happy life. Visit our online pharmacy, shop now, or find a store near you.'
     ),
     OffCampus(
-      name: 'Stop & Shop #2633',
-      address:'150 Whalley Ave, New Haven, CT',
+      name: 'Stop & Shop',
+      position: LatLng(41.31452403443663, -72.93778720674716),
       phone: '203-503-0105',
       websiteUrl: 'https://stopandshop.com/',
       hours:'Monday-Saturday: 7:00AM-10:00PM \n Sunday: 7:00AM-9:00PM',
@@ -212,7 +199,7 @@ List<OffCampus> storeList = <OffCampus>[
     ),
      OffCampus(
       name: 'Subway',
-      address:'1461 Whalley Avenue, New Haven, CT',
+      position: LatLng(41.33446310984408, -72.975468803322),
       phone: '203-389-1520',
       websiteUrl: 'https://www.subway.com/en-US',
       hours:'Monday-Sunday: 9:00AM-10:00PM',
@@ -221,25 +208,17 @@ List<OffCampus> storeList = <OffCampus>[
     ),
      OffCampus(
       name: 'Three Brothers Diner',
-      address:'1038 Dixwell Ave, Hamden, CT',
+      position: LatLng(41.34165663970864, -72.93530403031046),
       phone: '203-777-9420',
       websiteUrl: 'https://www.threebrosdiner.com/',
       hours:'Sunday & Monday-Thursday: 6:00AM-9:00PM \n Friday & Saturday: 6:00AM-10:00PM',
       imageUrl: 'https://images.getbento.com/accounts/f8b22e66f35131ce9f56c1bfbaa4d9cd/media/images/20221three_brothers_diner_FINAL_Main.png',
       description: 'Easygoing diner offering American comfort fare such as burgers & breakfast items plus take-out.'
     ),
-     OffCampus(
-      name: 'White Stone Mini Mart',
-      address:'144 Arch Street, Hamden, CT',
-      phone: '203-772-4558',
-      websiteUrl: '',
-      hours:'',
-      imageUrl: 'https://s.hdnux.com/photos/01/25/41/77/22421495/5/1500x0.jpg',
-      description: ''
-    ),
+     
      OffCampus(
       name: 'Wing Madness',
-      address:'716 Dixwell Ave, New Haven, CT',
+      position: LatLng(41.3328656088956, -72.9354529438049),
       phone: '203-562-1188',
       websiteUrl: 'https://wingmadness.com/',
       hours:'Monday-Thursday: 11:00AM-10:00PM \n Friday & Saturday: 11:00AM-11:00PM \n Sunday: 12:00PM-10:00PM',
