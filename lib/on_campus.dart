e0478966223996d9b77e8b61222ad5174903e89c
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-16 17:13:38 -0400

    Update on_campus.dart

--- lib/on_campus.dart
+++ lib/on_campus.dart
@@ -1,6 +1,8 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class OnCampus {
   String name;
-  String address;
+  final LatLng position;
   String websiteUrl;
   String hours;
   String imageUrl;
@@ -8,7 +10,7 @@ class OnCampus {
 
  OnCampus(
       { required this.name,
-      required this.address,
+      required this.position,
       required this.websiteUrl,
       required this.hours,
       required this.imageUrl,
@@ -19,15 +21,15 @@ class OnCampus {
 List<OnCampus> storeList = <OnCampus>[
     OnCampus(
       name: 'Connecticut Dining Hall',
-      address:'Connecticut Hall',
+      position: LatLng(41.33269901116767, -72.95020430883642),
       websiteUrl: 'https://southernct.sodexomyway.com/dining-near-me/resident-dining',
       imageUrl: 'https://www.southernct.edu/sites/default/files/2018-08/SCSU_Conn%20Hall_12-8944.jpg',
-      hours: 'Monday-Friday: 7:30AM-9:00PM \n Saturday-Sunday: 10:00AM-9:00PM',
+      hours: 'Monday - Friday: 7:30AM-9:00PM \n Saturday-Sunday: 10:00AM-9:00PM',
       description:"Connecticut Dining Hall is proud to offer a dining program complete with signature brands and menu selections that include just about every item you can imagine. Just feast your eyes on what we have available! Featuring a wide variety of fresh food designed to satisfy everyone's appetite with food choices to rival your favorite restaurants."
     ),
     OnCampus(
       name: 'Adanti Student Center Food Court',
-      address:'Michael J. Adanti Student CenterT',
+      position: LatLng(41.33223453995379, -72.94868185582021),
       hours: 'Monday-Thursday: 11:00AM-6:30PM \n Friday: 11:00AM-2:30PM \n Saturday-Sunday: Closed',
       websiteUrl: 'https://southernct.sodexomyway.com/dining-near-me/adanti_student_center',
       imageUrl: 'https://www.southernct.edu/sites/default/files/2018-08/SCSU_17-AdantiFoodcourtResized-0913.jpg',
@@ -35,7 +37,7 @@ List<OnCampus> storeList = <OnCampus>[
     ),
     OnCampus(
       name: 'Bagel Wagon',
-      address:'295 Blake Street, New Haven, CT',
+      position: LatLng(41.3322925662294, -72.94646670976086),
       hours: 'Monday-Thursday: 11:00AM-6:30PM \n Friday: 7:30AM-2:30PM \n Saturday-Sunday: Closed',
       websiteUrl: 'https://southernct.sodexomyway.com/dining-near-me/bagel_wagon',
       imageUrl: 'https://content-service.sodexomyway.com/media/coffeebagel_tcm1054-23666.jpg?url=https://southernct.sodexomyway.com/',
@@ -43,7 +45,7 @@ List<OnCampus> storeList = <OnCampus>[
     ),
     OnCampus(
       name: 'Owl Perch',
-      address:'Buley Library',
+      position: LatLng(41.333385825049824, -72.948011887958),
       hours: 'Monday-Thursday: 7:30AM-6:30PM \n Friday-Sunday: Closed ',
       websiteUrl: 'https://southernct.sodexomyway.com/dining-near-me/owl_perch',
       imageUrl: 'http://crescentmagazine.org/wp-content/uploads/2019/04/pell_020119_owlperch-750x541.jpg',
@@ -51,7 +53,7 @@ List<OnCampus> storeList = <OnCampus>[
     ),
     OnCampus(
       name: "Roberta's Convenience Store",
-      address:'North Campus Hall',
+      position: LatLng(41.36265, -72.92419),
       hours: 'Monday-Thursday, Sunday: 3:00PM-10:00PM \n Friday-Saturday: Closed ',
       websiteUrl: 'http://crescentmagazine.org/top-5-campus-eateries',
       imageUrl: 'http://crescentmagazine.org/wp-content/uploads/2019/04/pell_020619_robertas2-750x471.jpg',
