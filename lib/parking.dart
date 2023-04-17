4e9ffa3a56125ec52ef1c6aa2d5352a481d85e64
Kevin-stapes <72505810+Kevin-stapes@users.noreply.github.com>
2023-04-17 18:21:55 -0400

    Update parking.dart

--- lib/parking.dart
+++ lib/parking.dart
@@ -110,33 +110,33 @@ class _GraphScreenState extends State<GraphScreen> {
 
 class _MyHomePageState extends State<MyHomePage> {
   static List<String> name = [
-    'Wintergreen Garage (North Campus)',
-    'West Campus Garage (West Campus)',
-    'Fitch Street Garage    (East Campus)',
-    'Parking Lot 3               (West Campus)',
-    'Parking Lot 9               (West Campus)'
+    'Wintergreen Garage \n(North Campus)',
+    'West Campus Garage \n(West Campus)',
+    'Fitch Street Garage  \n(East Campus)',
+    'Parking Lot 3  \n(West Campus)',
+    'Parking Lot 9  \n(West Campus)'
   ];
 
   static List garage = [
     'https://www.blcompanies.com/wp-content/uploads/2014/10/SCSUParkingGarageCT_htm_5f93e1f2.jpg',
     'https://inside.southernct.edu/sites/default/files/media-images/2022-01/GARAGE.jpg',
     'https://news.southernct.edu/wp-content/uploads/2020/03/Screen-Shot-2020-01-20-at-8.50.27-AM.png',
-    'https://se-images.campuslabs.com/clink/images/8d6ba122-69b6-47b6-851f-43935dd2c268a80a24d8-17ab-400d-97c5-8e2d3bf9ce1e.jpg?preset=med-sq',
+    'https://fastly.4sqi.net/img/general/width960/230059_KY9ItLvNmz17nEFizOGHIGWR4LU_Y1cN2izc5Pqq8ew.jpg',
     'https://inside.southernct.edu/sites/default/files/scsu_2018_solar_panels-6052.jpg'
   ];
 
   static List graph = [
-    'https://community.powerbi.com/t5/image/serverpage/image-id/296439iD4F17CD02CF31231/image-size/large?v=v2&px=999',
-    'https://community.powerbi.com/t5/image/serverpage/image-id/296439iD4F17CD02CF31231/image-size/large?v=v2&px=999',
-    'https://community.powerbi.com/t5/image/serverpage/image-id/296439iD4F17CD02CF31231/image-size/large?v=v2&px=999',
-    'https://community.powerbi.com/t5/image/serverpage/image-id/296439iD4F17CD02CF31231/image-size/large?v=v2&px=999',
-    'https://community.powerbi.com/t5/image/serverpage/image-id/296439iD4F17CD02CF31231/image-size/large?v=v2&px=999'
+    'images/WintergreenGraph.png',
+    'images/WestCampusGraph.png',
+    'images/FitchStreetGraph.png',
+    'images/Lot3Graph.png',
+    'images/Lot9Graph.png',
   ];
 
   static List address = [
     '11 Wintergreen Ave New Haven, Connecticut 06515 ',
+    'To left of Hickerson Hall ',
     'In front of Davis Hall ',
-    '100 Winter Green Ave ',
     'Behind Connecticut Hall on Farnham Ave ',
     'Across from Barak Obama Elementary on Farnham Ave '
   ];
@@ -150,26 +150,33 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          backgroundColor: Color.fromARGB(221, 2, 18, 200),
-
-          // leading: BackButton(onPressed: () => Navigator.pop(MyHomePage())),
-          title: const Text(
-            'Campus Parking Options',
-            style: ThemeText.otherAppBars,
-          ),
-        ),
+            backgroundColor: ThemeText.mainColor,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            centerTitle: true,
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(60.0), // Set the height you want
+              child: Container(
+                  child: const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text("Campus Parking",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600)),
+              )),
+            )),
         body: ListView.builder(
+            padding: EdgeInsets.fromLTRB(10, 50, 10, 25),
             itemCount: parkingdata.length,
             itemBuilder: (context, index) {
               return Card(
                 child: ListTile(
                   title: Text(parkingdata[index].name),
                   leading: SizedBox(
-                    width: 150,
+                    width: 160,
                     height: 150,
                     child: Image.network(parkingdata[index].garage),
                   ),
