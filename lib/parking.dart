37046a5da2b8f89ba152103d9693bdf584995884
Ryan Pollard <71097838+ryanpoll@users.noreply.github.com>
2023-04-17 19:19:37 -0400

    Update parking.dart

--- lib/parking.dart
+++ lib/parking.dart
@@ -1,114 +1,16 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
-import 'package:flutter_application_1/styles.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'styles.dart';
 import 'parking_model.dart';
 import 'parking_details.dart';
-import 'package:draw_graph/draw_graph.dart';
-import 'package:draw_graph/models/feature.dart';
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Parking());
-}
-
-class Parking extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Parking Page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class Parking extends StatefulWidget {
+  const Parking({super.key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class GraphScreen extends StatefulWidget {
-  @override
-  _GraphScreenState createState() => _GraphScreenState();
-}
-
-class _GraphScreenState extends State<GraphScreen> {
-  final List<Feature> features = [
-    Feature(
-      title: "Wintergreen",
-      color: Colors.blue,
-      data: [0.3, 0.6, 0.8, 0.9, 1, 1.2],
-    ),
-    Feature(
-      title: "West Campus",
-      color: Colors.black,
-      data: [1, 0.8, 0.6, 0.7, 0.3, 0.1],
-    ),
-    Feature(
-      title: "Fitch Street",
-      color: Colors.orange,
-      data: [0.4, 0.2, 0.9, 0.5, 0.6, 0.4],
-    ),
-    Feature(
-      title: "Lot 3",
-      color: Colors.red,
-      data: [0.5, 0.2, 0, 0.3, 1, 1.3],
-    ),
-    Feature(
-      title: "Lot 9",
-      color: Colors.green,
-      data: [0.25, 0.6, 1, 0.5, 0.8, 1, 4],
-    ),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white54,
-      appBar: AppBar(
-        title: Text("Parking Capacity Graph"),
-        automaticallyImplyLeading: false,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 64.0),
-            child: Text(
-              "Parking",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
-              ),
-            ),
-          ),
-          LineGraph(
-            features: features,
-            size: Size(420, 450),
-            labelX: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
-            labelY: ['25%', '45%', '65%', '75%', '85%', '100%'],
-            showDescription: true,
-            graphColor: Colors.black87,
-          ),
-          SizedBox(
-            height: 50,
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<Parking> {
   static List<String> name = [
     'Wintergreen Garage \n(North Campus)',
     'West Campus Garage \n(West Campus)',
