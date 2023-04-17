9b3137bea1859ba5c3cfde8089df5deec85d14ee
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-17 18:56:28 -0400

    Update food.dart

--- lib/food.dart
+++ lib/food.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'styles.dart';
 import 'foodPlacesInfo.dart';
 import 'foodDetails.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class Food extends StatelessWidget {
@@ -10,7 +11,11 @@ class Food extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(221, 2, 18, 200),
-        title: const Text('Food', style: ThemeText.otherAppBars),
+        centerTitle: true,
+        title: Text('Food & Dining', style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600)),
       ),
       body: ListView.builder(
             itemCount: foodList.length,
@@ -18,7 +23,7 @@ class Food extends StatelessWidget {
               FoodLocations food = foodList[index];
               return Card(
                 child: ListTile(
-                  title: Text(food.name),
+                  title: Text(food.name, style: GoogleFonts.lato(fontSize: 16, wordSpacing: 1, color: Colors.black)),
                   onTap: () {
                     Navigator.push(
                         context,
