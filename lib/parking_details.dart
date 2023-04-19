6571421cb633fc8a23d8416d679baac7414ef13a
Kevin-stapes <72505810+Kevin-stapes@users.noreply.github.com>
2023-04-19 18:57:48 -0400

    Update parking_details.dart

--- lib/parking_details.dart
+++ lib/parking_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/styles.dart';
+import 'package:flutter_application_1/other/styles.dart';
+import 'styles.dart';
 import 'parking_model.dart';
 import 'package:draw_graph/draw_graph.dart';
 import 'package:draw_graph/models/feature.dart';
@@ -12,72 +13,73 @@ class ParkingDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          backgroundColor: ThemeText.mainColor,
-          title: Text(parkingDataModel.name, style: ThemeText.otherAppBars),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          centerTitle: true,
+      appBar: AppBar(
+        backgroundColor: ThemeText.mainColor,
+        title: Text(parkingDataModel.name, style: ThemeText.otherAppBars),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.network(parkingDataModel.garage),
-              Image.asset(parkingDataModel.graph),
-              const SizedBox(
-                height: 25,
-                width: 25,
-              ),
-              Text(
-                parkingDataModel.desc,
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              )
-            ],
-          ),
-        ));
-  }
-}
-
-@override
-Widget build(BuildContext context) {
-  var features;
-  return Scaffold(
-    backgroundColor: Colors.white54,
-    appBar: AppBar(
-      title: Text("Parking Graph"),
-      automaticallyImplyLeading: false,
-    ),
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 64.0),
-          child: Text(
-            "Capacity",
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Parking Location',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                  ),
+                  child: Image.network(parkingDataModel.garage),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Busiest Times',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                  ),
+                  child: Image.asset(parkingDataModel.graph),
+                ),
+                SizedBox(height: 20),
+                Divider(),
+                SizedBox(height: 20),
+                Text(
+                  parkingDataModel.desc,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
             ),
           ),
-        ),
-        LineGraph(
-          features: features,
-          size: Size(420, 450),
-          labelX: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
-          labelY: ['25%', '45%', '65%', '75%', '85%', '100%'],
-          showDescription: true,
-          graphColor: Colors.black87,
-        ),
-        SizedBox(
-          height: 50,
-        )
-      ],
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
