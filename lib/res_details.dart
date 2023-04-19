3d3f03afe81ffc20f587bc73bb536eb9c93f35e9
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-19 18:17:11 -0400

    Update res_details.dart

--- lib/res_details.dart
+++ lib/res_details.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'styles.dart';
 import 'halls.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 
 
@@ -15,7 +18,10 @@ class ResidentDetails extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(221, 2, 18, 200),
         centerTitle: true,
-        title: Text(hall.name),
+        title: Text(hall.name, style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(0.0),
@@ -58,14 +64,25 @@ class ResidentDetails extends StatelessWidget {
                 indent: 0,
                 endIndent: 0,
               ),
+               
               Padding(
-                padding: const EdgeInsets.all(1.0),
-                child: Text(
-                  hall.websiteUrl.toString(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 17.0, fontStyle: FontStyle.italic),
-                ),
-              ), 
+                padding: const EdgeInsets.all(6.0),
+                child: InkWell(
+                    onTap: () {
+                     {
+                        launch(hall.websiteURL.toString());
+                      } 
+                    },
+                    child: Container(
+                      height: 20,
+                      alignment: Alignment.center,
+                      child: Text(hall.text.toString(),
+                          style: GoogleFonts.lato(
+                              fontSize: 17.0,
+                              decoration: TextDecoration.underline,
+                              color: ThemeText.mainColor)),
+                    )),
+              ),
               const Divider(
                 color: Color.fromARGB(255, 212, 209, 209),
                 height: 2.5,
