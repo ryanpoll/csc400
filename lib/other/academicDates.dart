1cb41e93bd3a957fd800e3e8019f0abb26f0ad43
Ryan Pollard <71097838+ryanpoll@users.noreply.github.com>
2023-04-20 11:06:33 -0400

    Update academicDates.dart

--- lib/other/academicDates.dart
+++ lib/other/academicDates.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
-import './other.dart';
-import './styles.dart';
+import 'other.dart';
+import 'styles.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
