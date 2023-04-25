33d813c79f93bdbef1d3ddeae43b24a998d0c732
Kevin-stapes <72505810+Kevin-stapes@users.noreply.github.com>
2023-04-25 18:36:46 -0400

    Update hootLootOn.dart

--- lib/other/hootLootOn.dart
+++ lib/other/hootLootOn.dart
@@ -5,7 +5,6 @@ import 'on_campus.dart';
 import 'store_details_on.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class HootLootOn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,21 +12,26 @@ class HootLootOn extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Color.fromARGB(221, 2, 18, 200),
           centerTitle: true,
-          title: Text('On Campus', style: GoogleFonts.lato(
+          title: Text('On Campus',
+              style: GoogleFonts.lato(
                   color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.w600)),
-          
         ),
-      
         body: ListView.builder(
             itemCount: storeList.length,
             itemBuilder: (context, index) {
               OnCampus store = storeList[index];
               return Card(
                 child: ListTile(
-                  title: Text(store.name, style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w600, wordSpacing: 1, color: Colors.black,)),
-                  leading: Image.network(store.imageUrl),
+                  title: Text(store.name,
+                      style: GoogleFonts.lato(
+                          fontSize: 16, wordSpacing: 1, color: Colors.black)),
+                  leading: Image.network(
+                    store.imageUrl,
+                    width: 85,
+                    height: 100,
+                  ),
                   trailing: Icon(Icons.arrow_forward_rounded),
                   onTap: () {
                     Navigator.push(
