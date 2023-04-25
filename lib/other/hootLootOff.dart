f1889f835c44409ec9a964fa29c1adc32c391d40
Kevin-stapes <72505810+Kevin-stapes@users.noreply.github.com>
2023-04-25 18:36:16 -0400

    Update hootLootOff.dart

--- lib/other/hootLootOff.dart
+++ lib/other/hootLootOff.dart
@@ -4,8 +4,6 @@ import 'off_campus.dart';
 import 'store_details_off.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-
 class HootLootOff extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,11 +11,11 @@ class HootLootOff extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Color.fromARGB(221, 2, 18, 200),
           centerTitle: true,
-          title: Text('Off Campus', style: GoogleFonts.lato(
+          title: Text('Off Campus',
+              style: GoogleFonts.lato(
                   color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.w600)),
-          
         ),
         body: ListView.builder(
             itemCount: storeList.length,
@@ -25,8 +23,15 @@ class HootLootOff extends StatelessWidget {
               OffCampus store = storeList[index];
               return Card(
                 child: ListTile(
-                  title: Text(store.name, style: GoogleFonts.lato(fontSize: 16, wordSpacing: 1, color: Colors.black,fontWeight: FontWeight.w600)),
-                  leading: Image.network(store.imageUrl),
+                  title: Text(store.name,
+                      style: GoogleFonts.lato(
+                          fontSize: 16, wordSpacing: 1, color: Colors.black)),
+                  leading: Image.network(
+                    store.imageUrl,
+                    height: 100, 
+                    width: 100, 
+                    fit: BoxFit.cover, 
+                  ),
                   trailing: Icon(Icons.arrow_forward_rounded),
                   onTap: () {
                     Navigator.push(
@@ -38,6 +43,5 @@ class HootLootOff extends StatelessWidget {
               );
             }));
   }
-  
 }
 
