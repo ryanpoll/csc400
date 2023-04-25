fa6678040fe08d06b50216e22db36b568bb9fe7b
nataliewright22 <98058433+nataliewright22@users.noreply.github.com>
2023-04-25 18:25:58 -0400

    Update hootLootOff.dart

--- lib/other/hootLootOff.dart
+++ lib/other/hootLootOff.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/material.dart';
-import 'parking.dart';
 import 'styles.dart';
-import 'on_campus.dart';
-import 'store_details_on.dart';
+import 'off_campus.dart';
+import 'store_details_off.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
-class HootLootOn extends StatelessWidget {
+
+class HootLootOff extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(221, 2, 18, 200),
           centerTitle: true,
-          title: Text('On Campus', style: GoogleFonts.lato(
+          title: Text('Off Campus', style: GoogleFonts.lato(
                   color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.w600)),
           
         ),
-      
         body: ListView.builder(
             itemCount: storeList.length,
             itemBuilder: (context, index) {
-              OnCampus store = storeList[index];
+              OffCampus store = storeList[index];
               return Card(
                 child: ListTile(
-                  title: Text(store.name, style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w600, wordSpacing: 1, color: Colors.black,)),
+                  title: Text(store.name, style: GoogleFonts.lato(fontSize: 16, wordSpacing: 1, color: Colors.black,fontWeight: FontWeight.w600)),
                   leading: Image.network(store.imageUrl),
                   trailing: Icon(Icons.arrow_forward_rounded),
                   onTap: () {
@@ -39,4 +38,6 @@ class HootLootOn extends StatelessWidget {
               );
             }));
   }
+  
 }
+
